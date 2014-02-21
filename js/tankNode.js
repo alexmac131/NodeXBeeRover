e417995b9972b0df4cf0ccf6e9e75141bfd0798d
Alex MacKinnon <alex.mackinnon2012@gmail.com>
2014-02-21 16:26:27 -0500

    updates to group same color css changes

--- js/tankNode.js
+++ js/tankNode.js
@@ -1,68 +1,59 @@
 
 $(function() {
 	event.preventDefault();
-        $("#forward").click(function(){
-        	changeIndicator('#forwardImg', 'images/forward_green.jpg');
-        	askForDataFromNode  ({color:"#ffffff",direction:"forward"});
-        	askForDataFromNode  ({tankData:1});
-	
-        }); 
-        $("#back").click(function(){
-               changeIndicator('#backImg', 'images/back_green.jpg');
-               askForDataFromNode  ({color:"#ffffff",direction:"back"});
-               askForDataFromNode  ({tankData:1});
-        });
-        $("#left").click(function(){
-        	changeIndicator('#leftImg', 'images/left_green.jpg');
-        		askForDataFromNode  ({color:"#ffffff",direction:"left"});
-               askForDataFromNode  ({tankData:1});
-        });
-        $("#right").click(function(){
-        	changeIndicator('#rightImg', 'images/right_green.jpg');
-        	askForDataFromNode ({color:"#ffffff",direction:"right"});
-               askForDataFromNode  ({tankData:1});
-        });
-        $("#stop").click(function(){
-		$("#status2").css("color", "#ff0000");
-                changeIndicator("", "");
-                askForDataFromNode ({color:"#ffffff",direction:"stop", stopevent:1});
-		$("#status1").css("color", "#DE6262");
-        });
+    $("#forward").click(function(){
+    	changeIndicator('#forwardImg', 'images/forward_green.jpg');
+    	askForDataFromNode  ({color:"#ffffff",direction:"forward"});
+    	askForDataFromNode  ({tankData:1});
+    }); 
+    $("#back").click(function(){
+           changeIndicator('#backImg', 'images/back_green.jpg');
+           askForDataFromNode  ({color:"#ffffff",direction:"back"});
+           askForDataFromNode  ({tankData:1});
+    });
+    $("#left").click(function(){
+    	changeIndicator('#leftImg', 'images/left_green.jpg');
+    	askForDataFromNode  ({color:"#ffffff",direction:"left"});
+        askForDataFromNode  ({tankData:1});
+    });
+    $("#right").click(function(){
+    	changeIndicator('#rightImg', 'images/right_green.jpg');
+    	askForDataFromNode ({color:"#ffffff",direction:"right"});
+        askForDataFromNode  ({tankData:1});
+    });
+    $("#stop").click(function(){
+		changeIndicator("", "");
+        askForDataFromNode ({color:"#ffffff",direction:"stop", stopevent:1});
+    });
 	$("#slider-vertical2").slider({
-      		orientation: "vertical",
-      		range: "min",
-      		min: 0,
-      		max: 255,
-      		value: 60,
-      		slide: function( event, ui ) {
-        		$( "#amount2" ).text( ui.value );
+  		orientation: "vertical",
+  		range: "min",
+  		min: 0,
+  		max: 255,
+  		value: 60,
+  		slide: function( event, ui ) {
+    		$( "#amount2" ).text( ui.value );
 			askForDataFromNode ({engine:ui.value,engineUpDate:"left"});
-      		},
+  		},
 		change: function( event, ui ) {
-			console.log ("slider event");
+			$( "#amount2" ).text( ui.value );
 		}
-    	});
-
-
-	$( "#slider-vertical2" ).on( "slidechange", function( event, ui ) {} ) ;
-
+    });
 
 	$("#slider-vertical3").slider({
-      		orientation: "vertical",
-      		range: "min",
-      		min: 0,
-      		max: 255,
-      		value: 60,
-      		slide: function( event, ui ) {
-        		$( "#amount3" ).text( ui.value );
-				askForDataFromNode ({engine:ui.value,engineUpDate:"right"});
-		    },
-			change: function( event, ui ) {
-				console.log ("slider event");
-			}
-    	});
-	$( "#slider-vertical3" ).on( "slidechange", function( event, ui ) {} ) ;
-
+  		orientation: "vertical",
+  		range: "min",
+  		min: 0,
+  		max: 255,
+  		value: 60,
+  		slide: function( event, ui ) {
+    		$( "#amount3" ).text( ui.value );
+			askForDataFromNode ({engine:ui.value,engineUpDate:"right"});
+	    },
+		change: function( event, ui ) {
+    		$( "#amount3" ).text( ui.value );
+		}
+	});
 });
 
 var idsetPrev = "";
@@ -100,37 +91,22 @@ function askForDataFromNode(data) {
 }
 
 function processFeedBack (data) {
-	
-	console.log ("\n\ndata test ---->" + data.range 	 + "-----\n\n\n");
-
 	$("#status1").text( data.range);
 	$("#status2").text( data.lastCommand );
 	if (data.alert) {
-		console.log("alert event");
-		$("#status3").css("color", "#FF6802");
-		$("#status1").css("color", "#DE6262");
-		$("#status2").css("color", "#DE6262");
+		$("#status1,#status2,#status3, #status4").css("color", "#FF6802");
 		$("#status3").text( data.alert);
 		changeIndicator("","");
 		return;
 	}	
 	else if (data.lastCommand == "stop") {
-		console.log("stop event");
 		$("#status2").text( data.lastCommand );
-		$("#status2").css("color", "#DE6262");
+		$("#status2,#status1").css("color", "#DE6262");
 	}
 	else {
 		console.log("regular or default event");
-		$("#status3").css("color", "#000000");
-		$("#status1").css("color", "#000000");
-		$("#status2").css("color", "#000000");
+		$("#status1,#status2,#status3").css("color", "#000000");
 		$("#slider-vertical2").slider("value",data.leftEngine);
 		$("#slider-vertical3").slider("value", data.rightEngine);
-        $( "#amount2" ).text(data.leftEngine );
-        $( "#amount3" ).text(data.rightEngine );
 	}
-	console.log ("-------  " + data.heart + "---" + data.lastCommand);
-	
-			
-			
 }
